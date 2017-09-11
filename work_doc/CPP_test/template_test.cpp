9f0def634360674af3f6b32b69c2d173c0b3bf20
wedone2 <790981950@qq.com>
2017-09-11 15:55:01 +0800

    今日合并进度

--- work_doc/CPP_test/template_test.cpp
+++ work_doc/CPP_test/template_test.cpp
@@ -1,4 +1,22 @@
 #include <iostream>
-#include <string>
+template<unsigned n>
+struct Factorial
+{
+	enum{
+		value = n * Factorial<n-1>::value
+	};
+};
+template<>
+struct Factorial<0>
+{
+	enum{
+		value = 1;
+	};
+};
 
-cl
+int main()
+{
+	std::cout<< Factorial<5>::value;
+	std::cout<< Factorial<10>::value<<endl;
+	return 0;
+}
